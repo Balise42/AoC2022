ff6f9a78e907b136027d84b6255302052af73972
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2022-12-04 08:13:29 +0100

    adding types, moving vars to consts

--- day04.ts
+++ day04.ts
@@ -1,17 +1,17 @@
 import * as fs from 'fs';
 import * as rd from 'readline'
 
-var reader = rd.createInterface(fs.createReadStream("D:\\Home\\projects\\adventcalendar\\2022\\data\\day04.txt"))
-var sum = 0;
-var sum2 = 0;
-const regex = /(\d+)-(\d+),(\d+)-(\d+)/
+const reader : rd.Interface = rd.createInterface(fs.createReadStream("D:\\Home\\projects\\adventcalendar\\2022\\data\\day04.txt"))
+var sum : number = 0;
+var sum2 : number = 0;
+const regex : RegExp = /(\d+)-(\d+),(\d+)-(\d+)/
 
 reader.on("line", (l: string) => {
-    var matches = l.match( regex );
-    var a = parseInt(matches[1]);
-    var b = parseInt(matches[2]);
-    var c = parseInt(matches[3]);
-    var d = parseInt(matches[4]);
+    const matches : RegExpMatchArray = l.match( regex );
+    const a : number = parseInt(matches[1]);
+    const b : number = parseInt(matches[2]);
+    const c : number = parseInt(matches[3]);
+    const d : number = parseInt(matches[4]);
 
     if ( ( a <= c && b >= d ) || ( a >= c && b <= d ) ) {
         sum++;
