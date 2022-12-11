9c0c0e7839126faa078207eda06e3011269e0086
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2022-12-11 10:13:57 +0100

    day 11 part 1

---
+++ day11.ts
@@ -0,0 +1,89 @@
+import * as fs from 'fs';
+import * as rd from 'readline'
+
+const reader = rd.createInterface(fs.createReadStream("D:\\Home\\projects\\adventcalendar\\2022\\data\\day11.txt"))
+
+class Monkey {
+    public id : number;
+    public holding : Array<number> = [];
+    public opType : string;
+    public operand : number;
+    public divisible : number;
+    public ifTrue : number;
+    public ifFalse : number;
+    public inspected : number = 0;
+
+    public throwItem() : Array<number> {
+        this.inspected++;
+        let item = this.holding.pop();
+        if ( this.opType === 'square' ) {
+            item = item * item;
+        } else if ( this.opType === '*' ) {
+            item = item * this.operand;
+        } else if ( this.opType === '+' ) {
+            item = item + this.operand;
+        }
+        item = Math.floor(item / 3);
+        if ( item % this.divisible === 0 ) {
+            return [ item, this.ifTrue ];
+        } else {
+            return [ item, this.ifFalse ];
+        }
+    }
+}
+
+class MonkeySituation {
+    public monkeys : Array<Monkey> = [];
+
+    public doRound() {
+        for ( let i = 0; i < this.monkeys.length; i++ ) {
+            while ( this.monkeys[i].holding.length > 0 ) {
+                const thrown = this.monkeys[i].throwItem();
+                this.monkeys[thrown[1]].holding.push(thrown[0]);
+            }
+        }
+    }
+}
+
+let sit = new MonkeySituation();
+
+reader.on("line", (l: string) => {
+    const toks = l.split( ':' );
+    if ( toks[0].startsWith('Monkey')) {
+        let monkey = new Monkey();
+        monkey.id = sit.monkeys.length;
+        sit.monkeys.push( monkey );
+    } else {
+        let monkey = sit.monkeys[sit.monkeys.length - 1];
+        if ( toks[0].endsWith('Starting items')) {
+            const items = toks[1].split(', ');
+            for ( let item of items ) {
+                monkey.holding.push(parseInt(item));
+            }
+        } else if ( toks[0].endsWith('Operation')) {
+            if ( toks[1] === ' new = old * old' ) {
+                monkey.opType = 'square';
+            } else {
+                const groups = l.match( /old ([+|*]) (\d+)/ );
+                monkey.opType = groups[1];
+                monkey.operand = parseInt(groups[2]);
+            }
+        } else if ( toks[0].endsWith('Test')) {
+            monkey.divisible = parseInt(l.match( /\d+/ )[0]);
+        } else if ( toks[0].endsWith('If true')) {
+            monkey.ifTrue = parseInt(l.match( /\d+/ )[0]);
+        } else if ( toks[0].endsWith('If false')) {
+            monkey.ifFalse = parseInt(l.match( /\d+/ )[0]);
+        }
+    }
+});
+
+reader.on('close', () => {
+    for ( let i = 0; i < 20; i++ ) {
+        sit.doRound();
+    }
+
+    let sortedMonkeys = sit.monkeys.slice();
+    sortedMonkeys = sit.monkeys.sort( function(a, b) {return b.inspected - a.inspected});
+    console.log(sortedMonkeys[0].inspected * sortedMonkeys[1].inspected);
+});
